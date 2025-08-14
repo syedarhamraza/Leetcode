1f4a90f2c9f3ede6869730b4b2c30cc1b23a863e
syedarhamraza <arhamraza399@gmail.com>
2025-08-14 17:09:48 +0500

    Updated: Problem 136

--- 136.SingleNumber.cpp
+++ 136.SingleNumber.cpp
@@ -1,4 +1,36 @@
 //
 // Created by arham on 14/08/2025.
 //
+//Question
+/*Given a non-empty array of integers nums, every element appears twice except for one.
+Find that single one.
+You must implement a solution with a linear runtime complexity and use only constant extra space.*/
+
+//Sol
+// Using bitwise XOR where n is any number
+// n ^ n = 0
+// n ^ 0 = n
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int single_number = 0;
+        for(int value: nums) {
+            single_number = single_number ^ value;
+        }
+        return single_number;
+
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {4,1,2,1,2};
+    cout<< "The single number is: " << sol.singleNumber(nums);
+
+}
 
